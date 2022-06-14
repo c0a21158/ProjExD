5c207127ec5cb0083f294223b4567ed8c2a66645
yamaguchi <c0a211588c@edu.teu.ac.jp>
2022-06-14 15:47:57 +0900

    くいず

---
+++ ex01/quiz.py
@@ -0,0 +1,26 @@
+import random
+num = random.randint(0,3)
+
+if num == 0:
+    print("問題：サザエの旦那の名前は？")
+    x = input("")
+    if x == "ますお" or "マスオ":
+        print("正解")
+    else:
+        print("不正解")
+
+elif num == 1:
+    print("問題:カツオの娘の名前は？")
+    x = input("")
+    if x == "わかめ" or "ワカメ":
+        print("正解")
+    else:
+        print("不正解")
+        
+else:
+    print("問題：タラオはカツオから見てどんな関係？")
+    x = input("")
+    if x == "おい" or "甥" or "甥っ子" or "おいっこ":
+        print("正解")
+    else:
+        print("不正解")
