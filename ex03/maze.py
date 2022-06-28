7636c32c143800631e046829dad4afac8e1ce81d
yamaguchi <c0a211588c@edu.teu.ac.jp>
2022-06-28 15:52:31 +0900

    3限基本機能実装完了

---
+++ ex03/maze.py
@@ -0,0 +1,48 @@
+import tkinter as tk
+import maze_maker as mm
+
+def key_down(event):
+    global key
+    key = event.keysym
+    print(f"{key}キーが押されました")
+
+def key_up(event):
+    global key
+    key = ""
+
+def main_proc():
+    global cx, cy,mx,my
+    
+    delta = {                   #キー：押されているキーkey/値:移動幅リスト[x,y]
+        "Up"   :[0,-1],
+        "Down" :[0,+1],
+        "Left" :[-1,0],
+        "Right":[+1,0]
+    }
+    try:
+        if maze_bg[my+delta[key][1]][mx+delta[key][0]] == 0:
+            my, mx = my+delta[key][1] ,mx+delta[key][0]
+    except:
+        pass
+
+    cx, cy = mx*100+50 , my*100+50
+    canvas.coords("tori",cx,cy)
+    root.after(100,main_proc)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("迷えるこうかとん")
+    canvas = tk.Canvas(root, width= 1500,height=900,bg="black")
+    canvas.pack()
+    maze_bg = mm.make_maze(15,9)
+    mm.show_maze(canvas,maze_bg)
+
+    tori = tk.PhotoImage(file="ex03/fig/5.png")
+    mx ,my = 1,1
+    cx, cy = mx*100 + 50, my*100 +50
+    canvas.create_image(cx,cy,image=tori,tag="tori")
+    key = ""
+    root.bind("<KeyPress>" ,key_down)
+    root.bind("<KeyRelease>",key_up)
+    root.after(100,main_proc)
+    root.mainloop()
