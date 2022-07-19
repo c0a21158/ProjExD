a9bf8b6c6bfe0a26b5555c5c7e0708ec4ac4d8dc
yamaguchi <c0a211588c@edu.teu.ac.jp>
2022-07-19 16:40:29 +0900

    block

--- ex06/bbbb.py
+++ ex06/bbbb.py
@@ -13,7 +13,7 @@ can.place(x=10, y=10)
 
 block = []
 for x in range(8) :
-    for y in range(1) :
+    for y in range(2) :
         block.append({"x":x*75+5, "y":y*40+10, "st":1})
 
 def drawBlock() :
@@ -22,14 +22,9 @@ def drawBlock() :
         x = block[i]["x"]
         y = block[i]["y"]
         st = block[i]["st"]
-    #     if ball_y<=y+30 and ball_x>=x-10 and ball_x<=x+60 and st==1 :
-    #         by *= -1
-    #         block[i]["st"] = 0
+
         if st==1 :
             can.create_rectangle(x, y, x+70, y+30, fill="white")
-            block_count += 1
-    # if block_count == 0:
-    #     exit()
 
 def gameLoop():
     can.delete("all")
